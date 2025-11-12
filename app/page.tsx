17f9df2a5e1ad7f64a2e7300f0b368ac3904c74b
kimberlyflowers <42077154+kimberlyflowers@users.noreply.github.com>
2025-11-12 17:49:58 -0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>🌼 BloomShield Prototype</h1>
+      <p>Digital Content Protection Platform</p>
+      <p>File upload system coming soon!</p>
+    </div>
+  );
+}
